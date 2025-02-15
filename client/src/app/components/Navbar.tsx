457db55d64f97ef4003cd9b4ccce1fa30e359a36
jordanwu <89536016+WuChihWei@users.noreply.github.com>
2025-02-15 13:03:06 +0100

    inspect user login status

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.css';
+import { auth } from '../firebase/firebaseConfig';
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setIsLoggedIn(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -11,9 +23,17 @@ const Navbar = () => {
       <div className={styles.navItems}>
         {/* <a href="#how-it-works">How It Works</a> */}
         <a href="/">Home</a>
-        <Link href="/profile" className={styles.navLink}>My Profile</Link>
-        <Link href="/login" className={styles.loginButton}>Login</Link>
-        <Link href="/signup" className={styles.loginButton}>signup</Link>
+        {isLoggedIn ? (
+          <>
+            <Link href="/profile" className={styles.navLink}>My Profile</Link>
+            <button onClick={() => auth.signOut()} className={styles.loginButton}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link href="/login" className={styles.loginButton}>Login</Link>
+            <Link href="/signup" className={styles.loginButton}>Sign up</Link>
+          </>
+        )}
       </div>
     </header>
   );
